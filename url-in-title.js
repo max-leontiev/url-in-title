56655b6e70811b093b91cc9b18a4b2784f9d2667
max <mleontie@ualberta.ca>
2024-01-04 16:54:06 -0700

    Add timeout and remove page action tooltip changer

--- url-in-title.js
+++ url-in-title.js
@@ -1,25 +1,21 @@
-async function queryActiveTabs() {
-  return browser.tabs.query({active: true, lastFocusedWindow: true})
-}
-
-async function updateActionTooltip() { // update extension action tooltip if tab title contains the URL already
-  tab = (await queryActiveTabs())[0]
-  if (tab.title.endsWith(" - " + tab.url)) {
-    browser.action.setTitle({title: "Tab title contains URL", tabId: tab.id})
+function addAndRemoveURL() {
+  if (!document.title.endsWith(" - " + window.location.href)) { // append URL to title if it isn't there already
+    document.title += " - " + window.location.href
   }
+  setTimeout(() => { // remove URL from title after 1 second
+    if (document.title.endsWith(" - " + window.location.href)) { 
+      document.title = document.title.slice(0, -(" - ".concat(window.location.href).length))
+    }
+  }, 1000)
 }
 
-async function addURLtoTabTitle(tab) {
+async function addURLtoTitle(tab) {
   await browser.scripting.executeScript({
     target: {
       tabId: tab.id,
     },
-    func: () => {
-      if (!document.title.endsWith(" - " + window.location.href)) {
-        document.title += " - " + window.location.href
-      }
-    }
-  }).then(updateActionTooltip) // update extension action tooltip to reflect current state
+    func: addAndRemoveURL
+  })
 }
 
 if (window.matchMedia('(prefers-color-scheme: dark').matches) { // color icon based on color scheme preference
@@ -28,9 +24,9 @@ if (window.matchMedia('(prefers-color-scheme: dark').matches) { // color icon ba
   })
 }
 
-browser.action.onClicked.addListener(addURLtoTabTitle) // when extension action is clicked, append URL to tab title if it isn't already there
+browser.action.onClicked.addListener(addURLtoTitle) // when extension action is clicked, append URL to tab title if it isn't already there
 browser.commands.onCommand.addListener(async (command) => { // when keyboard shortcut is activated
   if (command === "Add URL to title") {
-    addURLtoTabTitle((await queryActiveTabs())[0])
+    addURLtoTitle((await browser.tabs.query({active: true, lastFocusedWindow: true}))[0])
   }
 });
