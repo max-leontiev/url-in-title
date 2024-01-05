1cf74cf5a8b070972596b4eb1542c846b99b79df
max <mleontie@ualberta.ca>
2024-01-04 17:40:20 -0700

    Add copyright and GPLv3 disclaimer to source file

--- url-in-title.js
+++ url-in-title.js
@@ -1,3 +1,18 @@
+/*
+URL in Title - a simple Firefox extension that lets you add the current tab's URL to its title with the click of a button.
+Copyright (C) 2023 Max Leontiev
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. 
+If not, see <https://www.gnu.org/licenses/>. 
+*/
+
 function addAndRemoveURL() {
   if (!document.title.endsWith(" - " + window.location.href)) { // append URL to title if it isn't there already
     document.title += " - " + window.location.href
